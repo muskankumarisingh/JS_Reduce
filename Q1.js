a83e1f3903384525927f4243f75b7e9ac25571e9
muskankumarisingh <81345123+muskankumarisingh@users.noreply.github.com>
2021-10-16 10:19:11 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,6 @@
+let arr=[8,2,3,4,5,6];
+let sum= arr.reduce((accum,curr)=>{
+    return accum
+})
+console.log(sum);
+//8
---
+++ Q2.js
@@ -0,0 +1,6 @@
+let arr=[8,2,3,4,5,6];
+let sum= arr.reduce((accum,curr)=>{
+    return accum+curr;
+})
+console.log(sum);
+//28
